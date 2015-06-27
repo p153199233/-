03aa517528e7780c582c541a7ffd3b6737c18488
pangqian <153199233@qq.com>
2015-06-27 23:22:12 +0800

    aaa

---
+++ aaa/aaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaa
+//
+//  Created by 庞谦 on 15/6/27.
+//  Copyright (c) 2015年 pangqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaa/aaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaa
+//
+//  Created by 庞谦 on 15/6/27.
+//  Copyright (c) 2015年 pangqian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
